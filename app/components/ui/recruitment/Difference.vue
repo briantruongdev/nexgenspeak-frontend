dbaa2fbf9e5f23facee080413e419910f1afdec6
Ngọ Đức Cảnh <canh.nexgenspeak@gmail.com>
2026-01-27 15:08:59 +0700

    feat: add UiRecruitmentDifference component to recruitment page for enhanced user differentiation

---
+++ app/components/ui/recruitment/Difference.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container pt-24">
+    <p class="title">NexGen Speak và sự khác biệt</p>
+    <div class="grid grid-cols-[1fr_2fr] gap-11 mt-12">
+      <div class="flex flex-col justify-between">
+        <p class="bg-bg-primary p-6 rounded-2xl">
+          Chúng tôi tạo khác biệt bằng một thương hiệu đột phá. Chúng tôi tin rằng từng cá nhân sẽ trở nên xuất sắc hơn khi phát
+          huy được sức mạnh của tập thể. Tư duy đột phá sẽ giúp chúng tôi trở thành người mở lối.
+        </p>
+        <BaseButton text="Câu chuyện Thương Hiệu" class-name="rounded">
+          <BaseIcon name="arrow-right-white" class="shrink-0 text-white" />
+        </BaseButton>
+      </div>
+      <img src="/images/people2.png" alt="" class="h-[343px] w-full object-cover rounded-2xl" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- app/pages/recruitment.vue
+++ app/pages/recruitment.vue
@@ -3,6 +3,7 @@
 <template>
   <div>
     <UiRecruitmentBanner />
+    <UiRecruitmentDifference />
   </div>
 </template>
 
